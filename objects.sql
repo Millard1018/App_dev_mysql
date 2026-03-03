8212c96c9fc6f57111651afe9b1081f28e25600b
Millard1018 <155636660+Millard1018@users.noreply.github.com>
2026-03-03 15:29:04 +0800

    Add files via upload

---
+++ objects.sql
@@ -0,0 +1,206 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2026 at 08:24 AM
+-- Server version: 8.0.41
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `objects`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `birds`
+--
+
+CREATE TABLE `birds` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `care_taker_name` varchar(255) DEFAULT NULL,
+  `care_taker_age` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `birds`
+--
+
+INSERT INTO `birds` (`id`, `name`, `age`, `care_taker_name`, `care_taker_age`) VALUES
+(1, 'bird1', 10, 'millard1', '19'),
+(2, 'bird2', 10, 'millard2', '19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int NOT NULL,
+  `genre` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `book_year` int DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `genre`, `name`, `book_year`, `author`) VALUES
+(1, 'sci-fi', 'Dune', 1984, 'Frank Herbert'),
+(2, 'sci-fi', 'Dune: Messiah', 1990, 'Frank Herbert');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `id` int NOT NULL,
+  `brand_name` varchar(255) DEFAULT NULL,
+  `brand_item` varchar(255) DEFAULT NULL,
+  `average_cost` int DEFAULT NULL,
+  `buyer` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `brand_name`, `brand_item`, `average_cost`, `buyer`) VALUES
+(1, 'GUCCI', 'clothes', 100000, 'Frank Herbert'),
+(2, 'GUCCI', 'clothes', 100000, 'Frank Herbert'),
+(3, 'NIKE', 'shoe', 10000, 'Frank Herbert');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `owner_name` varchar(255) DEFAULT NULL,
+  `owner_age` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`id`, `name`, `age`, `owner_name`, `owner_age`) VALUES
+(1, 'toyota', 10, 'millard1', '19'),
+(2, 'honda', 10, 'millard2', '19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clothes`
+--
+
+CREATE TABLE `clothes` (
+  `id` int NOT NULL,
+  `cloth_name` varchar(255) DEFAULT NULL,
+  `cloth_type` varchar(255) DEFAULT NULL,
+  `average_cost` int DEFAULT NULL,
+  `buyer` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `clothes`
+--
+
+INSERT INTO `clothes` (`id`, `cloth_name`, `cloth_type`, `average_cost`, `buyer`) VALUES
+(1, 'SHEIN', 'clothes', 1000, 'Frank Herbert'),
+(2, 'POLO', 'clothes', 10000, 'Frank Herbert');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `birds`
+--
+ALTER TABLE `birds`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clothes`
+--
+ALTER TABLE `clothes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `birds`
+--
+ALTER TABLE `birds`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `clothes`
+--
+ALTER TABLE `clothes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
